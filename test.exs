3923ac29ff8d616884d421db04577747a1bde992
Luca Staffner <staffnerluca@gmail.com>
2024-05-21 05:36:00 +0200

    finished for presentation

--- test.exs
+++ test.exs
@@ -1,3 +1,10 @@
+numOfNums = IO.gets("number of numbers: ") 
+|> String.trim()
+|> String.replace("\n", "", global: true)
+|> String.to_integer()
+IO.puts numOfNums + 10
+
+"""
 test = 0
 test = 1
 IO.puts test
@@ -8,3 +15,4 @@ IO.puts b
 [a, b] = ["f", "u"]
 IO.puts a
 IO.puts b
+"""
